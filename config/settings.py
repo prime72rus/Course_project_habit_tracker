a1dfff7739df01959684aee04483574bb5af8afb
Dmitrii Grechko <prime.optimys@mail.ru>
2025-08-11 15:22:48 +0500

    test deploy

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
