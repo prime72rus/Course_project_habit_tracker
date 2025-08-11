44b8a3e49c76f3ee99c055108314a3ba2b62321f
Dmitrii Grechko <prime.optimys@mail.ru>
2025-08-11 15:10:55 +0500

    test deploy

--- config/settings.py
+++ config/settings.py
@@ -160,9 +160,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    # "DEFAULT_FILTER_BACKENDS": (
-    #     "django_filters.rest_framework.DjangoFilterBackend",
-    # ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
